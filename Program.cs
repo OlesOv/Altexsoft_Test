e969557c5d4828d4123e5abf05694ab856f6bd79
OlesOv <66430622+OlesOv@users.noreply.github.com>
2021-02-18 13:52:38 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Altexsoft_Test
+{
+    class Program
+    {
+        static bool isLucky(string number)
+        {
+            if (number.Length % 2 == 1) number = '0' + number;
+            int firstHalf = 0;
+            for (int i = 0; i < number.Length / 2; i++)
+            {
+                firstHalf += Convert.ToInt32(number[i]);
+            }
+            int secondHalf = 0;
+            for (int i = number.Length / 2; i < number.Length; i++)
+            {
+                secondHalf += Convert.ToInt32(number[i]);
+            }
+            return firstHalf == secondHalf;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter q for exit.\nOr enter a ticket number: ");
+            var input = Console.ReadLine();
+            while (input != "q")
+            {
+                if (isLucky(input))
+                {
+                    Console.WriteLine("This ticket number is lucky\n");
+                }
+                else
+                {
+                    Console.WriteLine("This ticket number is not lucky\n");
+                }
+                Console.Write("Enter q for exit.\nOr enter a ticket number: ");
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
